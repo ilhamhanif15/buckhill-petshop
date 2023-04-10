80d755a27a6d53f4646e7c60a758199e354da777
Hanif <ilhamhanif1512@gmail.com>
2023-04-10 22:01:25 +0700

    Add CRUD Functions in ProductService

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Models\Product;
 use App\Utils\Paginationable;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
 
 class ProductService
 {
@@ -15,10 +16,67 @@ class ProductService
         //
     }
 
+    /**
+     * @param Request $request
+     * @return Paginator
+     */
     public function getPaginate(Request $request)
     {
         $paginationable = new Paginationable($this->product, $request);
 
         return $paginationable->paginate();
+    }
+
+    /**
+     * @param array $data
+     * @return Product
+     */
+    public function store(array $data)
+    {
+        return DB::transaction(fn() => $this->product->create($data));
+    }
+
+    /**
+     * @param string $uuid
+     * @return Product|null
+     */
+    public function show(string $uuid)
+    {
+        return $this->product->where('uuid', $uuid)->first();
+    }
+
+    /**
+     * @param array $data
+     * @param string $uuid
+     * @return Product
+     */
+    public function update(array $data, string $uuid)
+    {
+        $productFound = $this->show($uuid);
+
+        if (!$productFound) {
+            abort(404, "Product Not Found.");
+        }
+
+        return DB::transaction(fn() => tap($productFound)->update($data)->refresh());
+    }
+
+    /**
+     * @param string $uuid
+     * @return Product
+     */
+    public function destroy(string $uuid)
+    {
+        $productFound = $this->show($uuid);
+
+        if (!$productFound) {
+            abort(404, "Product Not Found.");
+        }
+
+        $deletedData = $productFound;
+
+        DB::transaction(fn() => $productFound->delete());
+
+        return $deletedData;        
     }
 }
