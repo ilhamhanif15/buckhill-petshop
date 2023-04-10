d64be85ac5b4270a4e34abcf8a9ddaf96fdabd90
Hanif <ilhamhanif1512@gmail.com>
2023-04-10 15:06:24 +0700

    Add Product Model & Migration

---
+++ database/migrations/2023_04_10_075000_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->uuid('category_uuid');
+            $table->foreign('category_uuid')->references('uuid')->on('categories');
+            $table->string('title');
+            $table->float('price');
+            $table->text('description');
+            $table->json('metadata');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
