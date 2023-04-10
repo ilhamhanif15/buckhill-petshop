9dc99886f1da131dafe7f005596cb0ca32625883
Hanif <ilhamhanif1512@gmail.com>
2023-04-10 21:55:47 +0700

    add cast metadata and relation category in Product Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Product extends Model
 {
@@ -29,5 +30,22 @@ class Product extends Model
         'metadata',
     ];
 
-    
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'metadata' => 'array',
+    ];
+
+    /**
+     * Get the category associated with the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function category(): HasOne
+    {
+        return $this->hasOne(Category::class, 'category_uuid', 'uuid');
+    }
 }
