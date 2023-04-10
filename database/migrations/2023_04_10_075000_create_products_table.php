49dcd891bb03d2bbde9611e6d864216320f59d82
Hanif <ilhamhanif1512@gmail.com>
2023-04-10 15:07:17 +0700

    Add Constraint unique on every uuid column table

--- database/migrations/2023_04_10_075000_create_products_table.php
+++ database/migrations/2023_04_10_075000_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->uuid('uuid');
+            $table->uuid('uuid')->unique();
             $table->uuid('category_uuid');
             $table->foreign('category_uuid')->references('uuid')->on('categories');
             $table->string('title');
