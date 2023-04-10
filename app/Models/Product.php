6c24c85d61506a52a7dc7b23b890318ae66c67bc
Hanif <ilhamhanif1512@gmail.com>
2023-04-10 15:08:32 +0700

    Add Fillable and Uuids in Product Model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
+use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    use Uuids, HasFactory;
+
+    public function getUuidColName()
+    {
+        return 'uuid';
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'uuid',
+        'category_uuid',
+        'title',
+        'price',
+        'description',
+        'metadata',
+    ];
+
+    
 }
