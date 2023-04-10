7c5283d0fd712e3de5afdc08bf0c5a3d746320f7
Hanif <ilhamhanif1512@gmail.com>
2023-04-10 21:57:53 +0700

    Add comment Type hint for CRUD functions in CategoryService

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Models\Category;
 use App\Utils\Paginationable;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
 
@@ -17,6 +18,10 @@ class CategoryService
         //
     }
 
+    /**
+     * @param Request $request
+     * @return Paginator
+     */
     public function getPaginate(Request $request)
     {
         $paginationable = new Paginationable($this->category, $request);
@@ -24,6 +29,10 @@ class CategoryService
         return $paginationable->paginate();
     }
 
+    /**
+     * @param array $data
+     * @return Category
+     */
     public function store(array $data)
     {
         $existedCategorySlug = Category::where('slug', $data['slug'])->limit(1)->first();
@@ -37,11 +46,20 @@ class CategoryService
         return DB::transaction(fn() => $this->category->create($data));
     }
 
+    /**
+     * @param string $uuid
+     * @return Category|null
+     */
     public function show(string $uuid)
     {
         return $this->category->where('uuid', $uuid)->first();
     }
 
+    /**
+     * @param array $data
+     * @param string $uuid
+     * @return Category
+     */
     public function update(array $data, string $uuid)
     {
         $categoryFound = $this->show($uuid);
@@ -53,6 +71,10 @@ class CategoryService
         return DB::transaction(fn() => tap($categoryFound)->update($data)->refresh());
     }
 
+    /**
+     * @param string $uuid
+     * @return Category
+     */
     public function destroy(string $uuid)
     {
         $categoryFound = $this->show($uuid);
