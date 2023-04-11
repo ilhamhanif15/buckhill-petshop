53f66f394403f789ad0ce162ce5292617c054991
Hanif <ilhamhanif1512@gmail.com>
2023-04-11 22:38:27 +0700

    Add ProductServiceTest: test create, update, delete, and show

---
+++ tests/Unit/ProductServiceTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Category;
+use App\Models\Product;
+use App\Services\ProductService;
+use Illuminate\Support\Arr;
+use Tests\TestCase;
+use Illuminate\Support\Str;
+
+class ProductServiceTest extends TestCase
+{
+    public function _create_product()
+    {
+        $category = Category::create([
+            'title' => 'Category Test',
+            'slug' => Str::slug('Category Test')
+        ]);
+
+        $data = [
+            'category_uuid' => $category->uuid,
+            'title' => 'Product Test',
+            'price' => 100,
+            'description' => 'Product Test Description',
+            'metadata' => [],
+        ];
+
+        /** @var ProductService $productService */
+        $productService = app(ProductService::class);
+
+        $product = $productService->store($data);
+
+        return [
+            $product,
+            $category,
+            $data,
+            $productService
+        ];
+    }
+
+    public function test_create_new_product(): void
+    {
+        /** @var ProductService $productService */
+        [$product, $category, $data] = $this->_create_product();
+
+        $this->assertTrue($product->category_uuid === $data['category_uuid']);
+        $this->assertTrue($product->title === $data['title']);
+        $this->assertTrue($product->price === $data['price']);
+        $this->assertTrue($product->description === $data['description']);
+
+        $product->forceDelete();
+        $category->forceDelete();
+    }
+
+    public function test_update_a_product(): void
+    {   
+        /** @var ProductService $productService */
+        [$product, $category, $data, $productService] = $this->_create_product();
+
+        $updatedProduct = $productService->update([
+            'title' => 'Product Update Test'
+        ], $product->uuid);
+
+        $this->assertTrue($updatedProduct->title === 'Product Update Test');
+
+        $product->forceDelete();
+        $category->forceDelete();
+    }
+
+    public function test_delete_a_product(): void
+    {   
+        /** @var ProductService $productService */
+        [$product, $category, $data, $productService] = $this->_create_product();
+
+        $deletedProduct = $productService->destroy($product->uuid);
+
+        $this->assertEmpty(Product::where('uuid', $deletedProduct->uuid)->first());
+
+        $category->forceDelete();
+    }
+
+    public function test_show_a_product()
+    {
+        /** @var ProductService $productService */
+        [$product, $category, $data, $productService] = $this->_create_product();
+
+        $showProduct = $productService->show($product->uuid);
+
+        $this->assertNotEmpty($showProduct);
+
+        $product->forceDelete();
+        $category->forceDelete();
+    }
+}
